e31d277603f4bcbc37f8a3604851cf6c09be874d
EcarusRW <albertoveoveo@gmail.com>
2019-05-08 21:49:41 +0200

    Improved player movement

--- pixel/characters/Player.gd
+++ pixel/characters/Player.gd
@@ -5,34 +5,43 @@ var velocidad
 var acel
 var cansancio
 var exhausto
-var validMoveset=[
-"Walkup",
-"Walkdown",
-"Walkleft",
-"Walkright",
-"Walkupleft",
-"Walkupright",
-"Walkdownleft",
-"Walkdownright"]
+var validMoveset
+var direccion
+var pressed_keys
 
 func _ready():
 	exhausto=false
 	cansancio=0
 	acel=5
 	velocidad=100
+	validMoveset=[
+		"Walkup",
+		"Walkdown",
+		"Walkleft",
+		"Walkright",
+		"Walkupleft",
+		"Walkupright",
+		"Walkdownleft",
+		"Walkdownright"]
 
 func _physics_process(_delta):
 	#Movimiento
-	var direccion=Vector2()
+	direccion=Vector2()
+	pressed_keys=0
 	if Input.is_action_pressed("ui_up"):
 		direccion += Vector2(0, -1)
+		pressed_keys+=1
 	if Input.is_action_pressed("ui_down"):
 		direccion += Vector2(0, 1)
+		pressed_keys+=1
 	if Input.is_action_pressed("ui_left"):
 		direccion += Vector2(-1, 0)
+		pressed_keys+=1
 	if Input.is_action_pressed("ui_right"):
 		direccion += Vector2(1, 0)
-
+		pressed_keys+=1
+	if pressed_keys>2:
+		direccion=Vector2(0,0)
 	#Sprint
 	if !exhausto:
 		if Input.is_action_pressed("ui_sprint") && direccion!=Vector2(0,0):
@@ -48,8 +57,10 @@ func _physics_process(_delta):
 			exhausto=true
 			velocidad=50
 			$CooldownTimer.start()
+	
 	direccion= direccion.normalized()*velocidad
 	direccion=move_and_slide(direccion)
+
 func _input(event):
 	var state = "Walk"
 	if Input.is_action_pressed("ui_up"):
@@ -60,11 +71,11 @@ func _input(event):
 		state += "left"
 	if Input.is_action_pressed("ui_right"):
 		state += "right"
-	if state!="Idle" && state in validMoveset:
+	if state in validMoveset:
 		animations.play(state)
 	else:
 		animations.stop()
-		
+
 func _process(_delta):
 	pass
 
