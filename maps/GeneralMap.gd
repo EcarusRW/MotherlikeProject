2debcab81d5d6363aa40a70c6346d07a6eb9fb3b
EcarusRW <albertoveoveo@gmail.com>
2019-05-23 01:50:41 +0200

    Zone for spawning items, room collisions

--- maps/GeneralMap.gd
+++ maps/GeneralMap.gd
@@ -4,6 +4,8 @@ var items
 
 func _ready():
 	items=[]
+	randomize()
+	populate_with_items()
 
 func _on_Area2D_body_entered(body):
 	if body is RigidBody2D:
@@ -14,3 +16,18 @@ func _on_Area2D_body_exited(body):
 	if body is RigidBody2D:
 		items.erase(body)
 		$Label.text=str(items)
+
+func populate_with_items():
+	var n_items=randi()%11+1
+	var root=$"."
+	var box=preload("res://pixel/objects/Box.tscn")
+	for vec in $SpawnArea.polygon:
+		var instanced_box=box.instance()
+		instanced_box.position=vec+$SpawnArea.position
+		root.add_child(instanced_box)
+		$Label.text+=str(vec)
+	#for n in range(0,n_items):
+	#	var instanced_box=box.instance()
+	#	instanced_box.position=Vector2(30,30)
+	#	root.add_child(instanced_box)
+		
